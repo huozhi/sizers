79545856c4bb852c45d0c5848753481fdfbd5ef5
Jiachi Liu <inbox@huozhi.im>
2021-11-28 00:46:02 +0100

    add swc minify

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,74 @@
-use std::io::prelude::*;
 use flate2::Compression;
+use std::io::prelude::*;
+use anyhow::{Error};
 use flate2::write::ZlibEncoder;
+use std::sync::Arc;
+use swc::config::util::{BoolOrObject};
+use swc::common::{FileName, FilePathMapping, SourceMap};
+use swc::ecmascript::minifier::option::terser::{TerserEcmaVersion};
+use swc::{
+  config::{JsMinifyOptions, JsMinifyFormatOptions},
+  try_with_handler, Compiler,
+};
 
-
-pub struct CompressResult {
-  pub raw: u128,
-  pub gzip: u128
+pub struct CompressOutput {
+  pub origin: u128,
+  pub gzip: u128,
+  pub minified: u128
 }
 
-pub fn minify(code: &String) -> CompressResult {
-  let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+fn compiler() -> Arc<Compiler> {
+  let cm = Arc::new(SourceMap::new(FilePathMapping::empty()));
+  Arc::new(Compiler::new(cm))
+}
 
-  let raw_bytes = code.as_bytes();
-  match encoder.write_all(raw_bytes) {
-      Ok(_) => {},
-      Err(e) => panic!("{:?}", e)
-  }
+pub fn swc_minify(s: &str) -> Result<String, Error> {
+  let c = compiler();
+
+  try_with_handler(c.cm.clone(), false, |handler| {
+    let opts = JsMinifyOptions {
+      format: JsMinifyFormatOptions::default(),
+      toplevel: false,
+      keep_fnames: false,
+      keep_classnames: false,
+      ecma: TerserEcmaVersion::Num(2016),
+      mangle: BoolOrObject::Bool(false),
+      compress: BoolOrObject::Bool(false),
+      inline_sources_content: false,
+      output_path: Option::None,
+      module: false,
+      safari10: false,
+      source_map: BoolOrObject::Bool(false),
+    };
+    
+
+    let fm = c.cm.new_source_file(FileName::Anon, s.into());
+    let program = c
+      .minify(fm, &handler, &opts)
+      .expect("failed to minify");
+    
+    Ok(program.code)
+  })
+}
+
+pub fn compress(code: &String) -> CompressOutput {
+  let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+  let minified_code = swc_minify(&code).expect("failed to minify");
+  let minified_bytes = minified_code.as_bytes();
   
+  match encoder.write_all(minified_bytes) {
+    Ok(_) => {}
+    Err(e) => panic!("{:?}", e),
+  }
+
   let compressed_bytes = match encoder.finish() {
-      Ok(bytes) => bytes.len(),
-      Err(e) => panic!("{:?}", e)
+    Ok(bytes) => bytes.len(),
+    Err(e) => panic!("{:?}", e),
   };
 
-  CompressResult {
-    raw: raw_bytes.len() as u128,
-    gzip: compressed_bytes as u128
+  CompressOutput {
+    origin: code.as_bytes().len() as u128,
+    minified: minified_bytes.len() as u128,
+    gzip: compressed_bytes as u128,
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::fs;
-use sizers::{minify, CompressResult};
+use sizers::{compress};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,9 +9,9 @@ fn main() {
     println!("Reading file: {}...", filename);
 
     let code = fs::read_to_string(filename).expect("Could not read file");
-    let compress_result = minify(&code);
+    let output = compress(&code);
 
-    let CompressResult { raw: raw_bytes, gzip: compressed_bytes } = compress_result;
-    println!("raw bytes {}", raw_bytes);
-    println!("gzip bytes {}", compressed_bytes);
+    println!("origin: {} bytes", output.origin);
+    println!("minified: {} bytes", output.minified);
+    println!("gzip: {} bytes", output.gzip);
 }
